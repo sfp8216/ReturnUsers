e8fe5382ae4bfb31eeb0bcda51c99cf9fce75197
sfp8216 <sfp8216p@gmail.com>
2021-08-12 11:04:33 -0400

    More styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,11 @@ import {HttpClientModule} from '@angular/common/http';
 import { AppComponent } from "./app.component";
 import { UsersComponent } from './users/users.component';
 import { HeaderComponent } from './component/shared/header/header.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [AppComponent, UsersComponent, HeaderComponent],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [BrowserModule, HttpClientModule, NgbModule],
   bootstrap: [AppComponent, UsersComponent,HeaderComponent]
 })
 export class AppModule {}
